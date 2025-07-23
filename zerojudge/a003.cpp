77be55a65d954a97acece7f89e288a175b66a02d
nch00sek <cpshs311357@cpshs.hcc.edu.tw>
2025-07-23 10:47:01 +0800

    sign

--- zerojudge/a003.cpp
+++ zerojudge/a003.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-using namespcae std;
-int main({
-    int a, b;
-})
+using namespace std;
+int main()
+{
+    int M, D;
+    cin >> M >> D;
+    
+    int S = (M * 2 + D) % 3;
+    if (S == 0)
+    cout << "普通\n";
+    if (S == 1)
+    cout << "吉\n";
+    if (S == 2)
+    cout << "大吉\n";
+    return 0;
+}
--- zerojudge/a004.cpp
+++ zerojudge/a004.cpp
@@ -1,14 +1,15 @@
-// ZeroJudge a004: 文文的求婚
-// 題目描述：輸入一個整數 n，輸出 1+2+...+n 的總和。
 #include <iostream>
 using namespace std;
+int main(){
+    
+int year;
+cin >> year;
+if (year/4== 0)
+    cout << "閏年" <<endl;
+else
+    cout<< "平年"<< endl;
+return 0; 
 
-int main()
-{
-    int n, sum = 0;
-    cin >> n;
-    for (int i = 1; i <= n; ++i)
-        sum += i;
-    cout << sum << endl;
-    return 0;
+
+
 }
